88bcdabf3eb4754b6ce9d69ec3057da9afd5bc05
jonathan401 <jonathangomina5049@gmail.com>
2022-06-29 15:37:46 +0100

    add functionality to removes a book from shelf

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,19 @@ class Shelf {
     this.allBooks = copied;
     return this.allBooks;
   };
+
+  // remove book from shelf method
+  removeBook = (book: Book): Book[] => {
+    if (this.allBooks.indexOf(book) === -1) {
+      console.log(`oops, ${book.title} was not found in your shelf`);
+    } else {
+      let newBooks = this.allBooks.filter(b => b !== book);
+      this.allBooks = newBooks;
+      console.log(`You removed '${book.title}' from your shelf`);
+      return this.allBooks;
+    }
+    return this.allBooks;
+  };
 }
 
 // test
@@ -32,4 +45,23 @@ let bookOne: Book = {
 };
 
 jonathanLibrary.addToShelf(bookOne);
+console.log(jonathanLibrary);
+
+// testing remove book from shelf feature;
+let bookTwo: Book = {
+  title: 'The Guardian',
+  author: 'John Grisham',
+  id: 2
+};
+
+let bookThree: Book = {
+  title: 'Hold Tight',
+  author: 'Harlen Cobben',
+  genre: 'Mystery/Thriller',
+  id: 3
+};
+
+jonathanLibrary.addToShelf(bookTwo);
+jonathanLibrary.addToShelf(bookThree);
+jonathanLibrary.removeBook(bookOne);
 console.log(jonathanLibrary);
